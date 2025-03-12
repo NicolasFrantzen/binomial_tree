802628cfb8e4da0beaf459743447e8971a1a269e
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2025-03-12 22:31:42 +0100

    Add main.rs

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+pub mod tree;
+mod binomial_tree;
+mod instruments;
+
+fn main() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    //use super::*;
+
+    #[test]
+    fn test_european1() {
+
+    }
+}
